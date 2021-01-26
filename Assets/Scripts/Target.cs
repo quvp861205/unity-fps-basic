a5ed2a94f13c60e92576f0b71a9fcf684fba85c4
unknown <quvp861205@gmail.com>
2021-01-26 11:12:27 -0700

    Proyecto simple fps unity

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 2f;
+
+    public void DamageReceived(float damageAmount)
+    {
+        health -= damageAmount;
+        if(health<=0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    public float damageAmount = 1f;
+    public float range = 150;
+    public Camera PlayerCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetButtonDown("Fire1") )
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if( Physics.Raycast(PlayerCam.transform.position, PlayerCam.transform.forward, out hit, range) )
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if( target != null )
+            {
+                target.DamageReceived(damageAmount);
+            }
+        }
+    }
+}
